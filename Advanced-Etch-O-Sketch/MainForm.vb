4a9d6f2666eae1d2e0b85381fbb44654374d6ea5
Ju8t1n203 <Juxin203@gmail.com>
2025-04-06 13:36:42 -0600

    Add project files.

---
+++ Advanced-Etch-O-Sketch/MainForm.vb
@@ -0,0 +1,26 @@
+﻿'Justin Bell
+'RCET3371
+'Spring 2025
+'link
+
+Option Explicit On
+Option Strict On
+Public Class MainForm
+    Private Sub SplashTimer_Tick(sender As Object, e As EventArgs) Handles SplashTimer.Tick
+        Spash.Close()
+        Me.Show()
+        SplashTimer.Enabled = False
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
